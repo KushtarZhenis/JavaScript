005dc1417613ef6f29d9fb98137586f04a981918
HI1012 <jengisqushtar@gmail.com>
2023-10-28 12:20:34 +0600

    1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// alert("Salem Alem");
+document.write("<h1>Hello World</h1>");
+console.log("Hello from console");
+
+let age = 18;
+let name = 'Kushtar';
+let isMale = true;
+let listInfoPerson = {
+
+};
+
+console.log(age);
+console.log(name);
+console.log(isMale);
+// console.log(infoPerson);
