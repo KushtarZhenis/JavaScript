c5d4198548af7bbabad372c79abb039b9e0ab6f8
HI1012 <jengisqushtar@gmail.com>
2023-10-31 11:38:52 +0600

    2

--- js/main.js
+++ js/main.js
@@ -1,38 +1,12 @@
-// alert("Salem Alem");
-document.write("<h1>Hello World</h1>");
-console.log("Hello from console");
-
-let listInfoPerson = {
-  kushtar: {
-    age: 18,
-    isMale: true,
-    school: "KBTU",
-  },
-  talant: {
-    age: 17,
-    isMale: false,
-    school: "KBTU",
-  },
-  p1: {
-    age: 17,
-    isMale: false,
-    school: "KBTU",
-  },
-  p2: {
-    age: 21,
-    isMale: true,
-    school: "KBTU",
-  },
-  p3: {
-    age: 19,
-    isMale: false,
-    school: "KBTU",
-  },
+const fibonacci = (n) => {
+  if (n == 0) {
+    return 0;
+  } 
+  else if (n == 1) {
+    return 1;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
-console.log(listInfoPerson);
-console.log(Number("Erlan"));   //NaN
-console.log(parseInt("Erlan")); //NaN
-console.log(Number("11a"));     //NaN
-console.log(parseInt("11a"));   //11
-
+let fib = fibonacci(40);
+console.log(fib);
