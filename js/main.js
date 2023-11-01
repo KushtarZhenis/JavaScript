0c678e499fd58bcdee8562949384c097bb61eb09
HI1012 <jengisqushtar@gmail.com>
2023-11-01 09:50:37 +0600

    3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
-const fibonacci = (n) => {
-  if (n == 0) {
-    return 0;
-  } 
-  else if (n == 1) {
-    return 1;
-  }
-  return fibonacci(n - 1) + fibonacci(n - 2);
-};
+// const fibonacci = (n) => {
+//   if (n == 0) {
+//     return 0;
+//   }
+//   else if (n == 1) {
+//     return 1;
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// };
 
-let fib = fibonacci(40);
-console.log(fib);
+// let fib = fibonacci(40);
+// console.log(fib);
+
+let n = 1476;
+let a = 0;
+let b = 1;
+let res = 0;
+
+for (let i = 0; i <= n; i++) {
+  if (i == 0) {
+    res = a;
+  } else if (i == 1) {
+    res = b;
+  } else {
+    res = a + b;
+    a = b;
+    b = res;
+  }
+}
+console.log(res);
+console.log(Number.MAX_VALUE);
+
+// let c = 0;
+
+// while(res < Number.MAX_VALUE) {
+//     res = a + b;
+//     a = b;
+//     b = res;
+//     c++;
+// }
+// console.log(a);
+// console.log(c);
+
